0f0ebad79bdbc9fa3d3a430350b248ad0fe69eb7
ripraprup10 <ripraprup10@gmail.com>
2019-10-14 11:05:47 +0200

    updated wave manager and fire

--- P5_Prototype/Assets/WaveManager.cs
+++ P5_Prototype/Assets/WaveManager.cs
@@ -40,42 +40,58 @@ public class WaveManager : MonoBehaviour
     {
         
         var whitePixels = 0;
-        var blackPixels = 0;
+        var coloredPixels = 0;
         for (int i = 0; i < wavePic.width; i++)
             for (int j = 0; j < wavePic.height; j++)
             {
                 Color pixel = wavePic.GetPixel(i, j);
 
                 
-                if (pixel == Color.black)
+                if (pixel == Color.red)
                 {
-                    spawnProjectile(i, j);
-                    blackPixels++;
+                    spawnProjectile(i, "top");
+                    coloredPixels++;
+                }
+
+                else if (pixel == Color.green)
+                {
+                    spawnProjectile(i, "mid");
+                    coloredPixels++;
+                }
+
+
+                else if (pixel == Color.blue)
+                {
+                    spawnProjectile(i, "bot");
+                    coloredPixels++;
                 }
                 else
                 {
+                    Debug.Log(pixel);
                     whitePixels++;
                 }
                    
             }
-        Debug.Log(string.Format("White pixels {0}, black pixels {1}", whitePixels, blackPixels));
+        Debug.Log(string.Format("White pixels {0}, colored pixels {1}", whitePixels, coloredPixels));
+        Debug.Log(wavePic.width);
+        Debug.Log(wavePic.height);
     }
 
-    void spawnProjectile(int x, int y)
+    void spawnProjectile(int x, string place)
     {
         GameObject pro = null;
-        // spawn a top, mid or bot projectile according to y and make it wait according to x
-        pro = Instantiate(Projectile, transform.position, Quaternion.identity);
+        // spawn a top, mid or bot projectile according to the string and make it wait according to x
+        pro = Instantiate(Projectile, transform.position, transform.rotation);
         pro.GetComponent<projectile>().waitTime = x;
-        switch (y)
+        switch (place)
         {
-            case 0:
+            case "bot":
                 pro.GetComponent<projectile>().setEnum("bot");
                 break;
-            case 1:
+            case "mid":
                 pro.GetComponent<projectile>().setEnum("mid");
                 break;
-            case 2:
+            case "top":
                 pro.GetComponent<projectile>().setEnum("top");
                 break;
 
--- P5_Prototype/Assets/projectile.cs
+++ P5_Prototype/Assets/projectile.cs
@@ -11,10 +11,15 @@ public class projectile : MonoBehaviour
     float timeToReachTarget;
     float t;
     enum ProjectileType { none, top, mid, bot };
+    float LaunchAngle = 10f;
+    Rigidbody rigid;
     ProjectileType pt;
+    
     // Start is called before the first frame update
     void Start()
     {
+       
+        
         if (pt == ProjectileType.top)
         {
             startpos = GameObject.FindGameObjectWithTag("StartTopProjectile");
@@ -38,7 +43,8 @@ public class projectile : MonoBehaviour
 
         timeToReachTarget = WaveManager.instance.timeToReachTarget;
         transform.position = startpos.transform.position;
-        
+        rigid = GetComponent<Rigidbody>();
+       // transform.rotation = Quaternion.LookRotation(rigid.velocity) * transform.rotation;
         StartCoroutine(GoTimer());
     }
 
@@ -50,6 +56,9 @@ public class projectile : MonoBehaviour
         {
             t += Time.deltaTime / timeToReachTarget;
             transform.position = Vector3.Lerp(startpos.transform.position, target.transform.position, t);
+
+            //Launch();
+
         }
         
        
@@ -59,8 +68,8 @@ public class projectile : MonoBehaviour
     {
         yield return new WaitForSeconds(waitTime);
         go = true;
-        MeshRenderer m = GetComponent<MeshRenderer>();
-        m.enabled = true;
+        //MeshRenderer m = GetComponent<MeshRenderer>();
+        //m.enabled = true;
     }
 
     public void setEnum(string enumSet)
@@ -82,5 +91,37 @@ public class projectile : MonoBehaviour
             pt = ProjectileType.none;
         }
 
+    }
+
+    void Launch()
+    {
+
+        transform.rotation = Quaternion.LookRotation(rigid.velocity);
+        // think of it as top-down view of vectors: 
+        //   we don't care about the y-component(height) of the initial and target position.
+        Vector3 projectileXZPos = new Vector3(transform.position.x, 0.0f, transform.position.z);
+        Vector3 targetXZPos = new Vector3(target.transform.position.x, 0.0f, target.transform.position.z);
+
+        // rotate the object to face the target
+        transform.LookAt(targetXZPos);
+
+        // shorthands for the formula
+        float R = Vector3.Distance(projectileXZPos, targetXZPos);
+        float G = Physics.gravity.y;
+        float tanAlpha = Mathf.Tan(LaunchAngle * Mathf.Deg2Rad);
+        float H = target.transform.position.y - transform.position.y;
+
+        // calculate the local space components of the velocity 
+        // required to land the projectile on the target object 
+        float Vz = Mathf.Sqrt(G * R * R / (2.0f * (H - R * tanAlpha)));
+        float Vy = tanAlpha * Vz;
+
+        // create the velocity vector in local space and get it in global space
+        Vector3 localVelocity = new Vector3(0f, Vy, Vz);
+        Vector3 globalVelocity = transform.TransformDirection(localVelocity);
+
+        // launch the object by setting its initial velocity and flipping its state
+        rigid.velocity = globalVelocity;
+       // bTargetReady = false;
     }
 }
