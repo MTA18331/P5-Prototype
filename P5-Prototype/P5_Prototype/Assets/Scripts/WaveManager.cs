d326d4281f6c2bcf13b1f0d29edfe5068fb4aeb0
ripraprup10 <ripraprup10@gmail.com>
2019-10-23 12:28:22 +0200

    all the stuff

---
+++ P5-Prototype/P5_Prototype/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,221 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+   
+    public static WaveManager instance = null;
+  
+    
+    public float timeToReachTarget;
+
+    public float waitTimeBeforeStart;
+    public float waitTimeBeforeEnd;
+    public GameObject EndCanvas;
+    public GameObject ProjectileDad;
+    public GameObject Projectile;
+    public bool GenerateRandomWaves;
+    public Texture2D[] waveTextures;
+
+   
+    public float flowMax;
+    public float CurrentFlow = 1f;
+    public float flowMin;
+    private List<float> AverageFlowList = new List<float>();
+    public float AverageFlow;
+    public float currentBlockCombo;
+    public float currentMissCombo;
+    public float flowblock;
+    public float flowmiss;
+    public float flowadjustamount;
+    public int maxWaves;
+    
+    int waitTime;
+
+    int wavenumber=0; // husk at første wave er nummer 0
+    public int[] blockamount = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    public int[] missamount = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+
+    void Awake()
+    {
+        if (instance == null)
+
+            instance = this;
+
+        else if (instance != this)
+
+            Destroy(gameObject);
+
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        StartCoroutine(Waves());
+    }
+
+    private void Update()
+    {
+        Flow();
+    }
+
+
+    void buildWave(Texture2D wavePic)
+    {
+        var weirdPixels = 0;
+        var coloredPixels = 0;
+        for (int i = 0; i < wavePic.width; i++)
+        {
+            for (int j = 0; j < wavePic.height; j++)
+            {
+                Color pixel = wavePic.GetPixel(i, j);
+
+
+                if (pixel == Color.red)
+                {
+                    spawnProjectile(i, "top");
+                    coloredPixels++;
+                }
+
+                else if (pixel == Color.green)
+                {
+                    spawnProjectile(i, "mid");
+                    coloredPixels++;
+                }
+
+
+                else if (pixel == Color.blue)
+                {
+                    spawnProjectile(i, "bot");
+                    coloredPixels++;
+                }
+                else
+                {
+                    weirdPixels++;
+                }
+            }
+        }
+        // Debug.Log(string.Format("weird pixels {0}, colored pixels {1}", weirdPixels, coloredPixels));
+
+    }
+
+    void spawnProjectile(float x, string place)
+    {
+        //GameObject pro = null;
+        GameObject pro = Instantiate(Projectile, transform.position, transform.rotation);
+        pro.GetComponent<projectile>().waitTime = x* CurrentFlow;
+        pro.GetComponent<projectile>().WaveNumber = wavenumber;
+        pro.transform.SetParent(ProjectileDad.transform);
+        
+ 
+        switch (place)
+        {
+            case "bot":
+                pro.GetComponent<projectile>().setEnum("bot");
+                break;
+            case "mid":
+                pro.GetComponent<projectile>().setEnum("mid");
+                break;
+            case "top":
+                pro.GetComponent<projectile>().setEnum("top");
+                break;
+
+            default:
+                Debug.Log("Well, shit");
+               
+                break;
+        }
+    }
+
+    void gameEnd()
+    {
+        CSV.instance.Save( blockamount, missamount);
+    }
+
+    void Flow()
+    {
+        if (currentBlockCombo % 3 == 1)
+        {
+            if (CurrentFlow > flowMin)
+            {
+                CurrentFlow -= 0.05f;
+                currentBlockCombo = 0;
+                currentMissCombo=0;
+              
+            }
+        }
+        if (currentMissCombo % 6 ==1)
+        {
+            if (CurrentFlow < flowMax)
+            {
+
+            CurrentFlow += 0.05f;
+            currentMissCombo=0;
+             currentBlockCombo = 0;
+            }
+        }
+        float tempFlow = 0;
+        for (int i = 0; i < AverageFlowList.Count; i++)
+        {
+            tempFlow += AverageFlowList[i];
+        }
+        AverageFlow = tempFlow / AverageFlowList.Count;
+    }
+
+    IEnumerator Waves()
+    {
+
+        if (wavenumber == 0)
+        {
+            yield return new WaitForSeconds(waitTimeBeforeStart);
+        }
+
+
+        Debug.Log("Send out wave " + wavenumber + " based on flow of " + CurrentFlow);
+        if (GenerateRandomWaves && wavenumber<maxWaves)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                int r = Random.Range(0, 2);
+                if (r == 0)
+                {
+                    spawnProjectile(i, "top");
+                }
+                else if (r == 1)
+                {
+                    spawnProjectile(i, "mid");
+                }
+                else if (r == 2)
+                {
+                    spawnProjectile(i, "mid");
+                }
+                
+                
+
+            }
+            wavenumber++;
+        } else if (wavenumber < maxWaves && !GenerateRandomWaves)
+        {
+
+           buildWave(waveTextures[wavenumber]);
+            wavenumber++;
+        }else
+        {
+            yield return new WaitForSeconds(waitTimeBeforeEnd);
+            EndCanvas.SetActive(true);
+            gameEnd();
+            StopCoroutine(Waves());
+        }
+
+        AverageFlowList.Add(CurrentFlow);
+        yield return new WaitForSeconds(CurrentFlow*10);
+        StartCoroutine(Waves());
+           
+
+        }
+    }
+
---
+++ P5-Prototype/P5_Prototype/Assets/Scripts/projectile.cs
@@ -0,0 +1,161 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    
+    public float waitTime;
+    public int WaveNumber;
+    public GameObject startpos;
+    public GameObject  Peak; 
+    public GameObject target;
+    
+    GameObject[] startPositions;
+    bool go = false;
+    float timeToReachTarget;
+    float t;
+    
+    enum ProjectileType { none, top, mid, bot };
+    ProjectileType pt;
+    
+    public float LaunchAngle = 60f;
+    Rigidbody rigid;
+    
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (pt == ProjectileType.top)
+        {
+            target = GameObject.FindGameObjectWithTag("PTop");
+        }
+        else if (pt == ProjectileType.mid)
+        {
+            target = GameObject.FindGameObjectWithTag("PMid");
+        }
+        else if (pt == ProjectileType.bot)
+        {
+            target = GameObject.FindGameObjectWithTag("PBot");
+        } else {
+            Debug.Log("problems with findings targets!");
+        }
+        startpos = RandomStartPos();
+        timeToReachTarget = WaveManager.instance.timeToReachTarget;
+        transform.position = startpos.transform.position;
+        Peak = startpos.transform.GetChild(0).gameObject;
+        GameObject.FindGameObjectWithTag("Peak");
+        rigid = GetComponent<Rigidbody>();
+       // transform.rotation = Quaternion.LookRotation(rigid.velocity) * transform.rotation;
+        //transform.rotation = Quaternion.LookRotation(transform.position);
+        //transform.rotation = Quaternion.LookRotation(-rigid.velocity);
+        StartCoroutine(GoTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+  
+        if (go)
+        {
+           //audiomanager.instance.PlayArrowStarting();
+            t += Time.deltaTime / timeToReachTarget;
+           //transform.position = Vector3.Lerp(startpos.transform.position, target.transform.position, t);
+            
+            Launch();
+
+        }
+        
+       
+    }
+
+    IEnumerator GoTimer()
+    {
+        yield return new WaitForSeconds(waitTime);
+        go = true;
+        //MeshRenderer m = GetComponent<MeshRenderer>();
+        //m.enabled = true;
+    }
+
+    public void setEnum(string enumSet)
+    {
+        if (enumSet == "top"){
+            pt = ProjectileType.top;
+        }
+        else if (enumSet == "mid"){
+            pt = ProjectileType.mid;
+        }
+        else if (enumSet == "bot"){
+            pt = ProjectileType.bot;
+        }
+        else{
+            pt = ProjectileType.none;
+        }
+    }
+
+    void Launch()
+    {
+        
+        //rigid.velocity = -startpos.transform.forward*5;
+        transform.position = (1.0f - t) * (1.0f - t) * startpos.transform.position + 2*(1-t)*t*Peak.transform.position+(t*t)*target.transform.position;
+        //transform.rotation = Quaternion.LookRotation(-rigid.velocity);
+        Vector3 direction = new Vector3(0,0,0);
+        direction.x = transform.position.x - target.transform.position.x;
+        direction.y = transform.position.y - target.transform.position.y;
+        direction.z = transform.position.z - target.transform.position.z;
+
+        float angle = Mathf.Atan2 (-direction.y, direction.x) * Mathf.Rad2Deg;
+        //transform.rotation = (0, angle, 0);
+        Vector3 ops= new Vector3(0, angle, 0)
+        transform.rotation = Quaternion.LookRotation( ops );
+    }
+
+    GameObject RandomStartPos()
+    {
+        try{
+            startPositions = GameObject.FindGameObjectsWithTag("startPosition");
+            int r = Random.Range(0, startPositions.Length);
+            //Debug.Log("spawning from " + startPositions[r].name);
+            return startPositions[r];
+        }
+        catch{
+            Debug.LogError("Projectiles failed to find starting positions");
+            return null;
+        }    
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.tag == "shield"){
+            Debug.Log("Executed");
+            go = false;
+            //audiomanager.instance.PlayArrowBlock();
+            WaveManager.instance.blockamount[WaveNumber] ++;
+            WaveManager.instance.currentBlockCombo ++;
+            WaveManager.instance.currentMissCombo = 0;
+            //transform.SetParent(collision.gameObject.transform);
+            //Destroy(gameObject, 10);
+            
+            transform.SetParent(collision.gameObject.transform);
+            Destroy(gameObject, 15);
+        }
+        else if (collision.gameObject.tag == "PTop" || collision.gameObject.tag == "PMid" || collision.gameObject.tag == "PBot")
+        {
+            //audiomanager.instance.PlayHurt();
+            WaveManager.instance.missamount[WaveNumber] ++;
+            WaveManager.instance.currentBlockCombo = 0;
+            WaveManager.instance.currentMissCombo ++;
+            go = false;
+            Destroy(gameObject);
+        }
+
+
+    }
+}
+
+
